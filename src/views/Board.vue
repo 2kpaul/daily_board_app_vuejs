f11e306b009304f4e3e6086622433b420cfb2302
Paul2D <paul.maxineanu@gmail.com>
2022-09-09 13:25:09 +0300

    drag and drop tasks

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -2,17 +2,24 @@
   <div class="board">
     <div class="flex flex-row items-start">
       <div class="column" 
-        v-for="(column, index) in board.columns" 
-        :key="index"
+        v-for="(column, columnIndex) in board.columns" 
+        :key="columnIndex"
+        @drop="moveTask($event, column.tasks)"
+        @dragover.prevent
+        @dragenter.prevent
       >
         <div class="flex items-center mb-2 font-bold">{{ column.name }}</div>
         <div class="list-reset">
           <div class="task" 
             v-for="(task, taskIndex) in column.tasks" 
             :key="taskIndex"
+            draggable="true"
+            @dragstart="pickupTask($event, taskIndex, columnIndex)"
             @click="goToTask(task)"
           >
-            <span class="w-full flex-shrink-0 font-bold">{{ task.name }}</span>
+            <span class="w-full flex-shrink-0 font-bold">
+              {{ task.name }}
+            </span>
             <p class="w-full flex-shrink-0 mt-1 text-sm" v-if="task.description">
               {{ task.description }}
             </p>
@@ -59,6 +66,23 @@ export default {
         name: e.target.value
       })
       e.target.value = ''
+    },
+    pickupTask(e, taskIndex, fromColumnIndex) {
+      e.dataTransfer.effectAllowed = 'move'
+      e.dataTransfer.dropEffect = 'move'
+      e.dataTransfer.setData('task-index', taskIndex)
+      e.dataTransfer.setData('from-column-index', fromColumnIndex)
+    },
+    moveTask(e, toTasks) {
+      const fromColumnIndex = e.dataTransfer.getData('from-column-index')
+      const fromTasks = this.board.columns[fromColumnIndex].tasks
+      const taskIndex = e.dataTransfer.getData('task-index')
+
+      this.$store.commit('MOVE_TASK', {
+        fromTasks,
+        toTasks,
+        taskIndex
+      })
     }
   }
 }
