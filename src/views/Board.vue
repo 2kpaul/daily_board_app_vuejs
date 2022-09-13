9259490e4e2f3aaa1537e0d057c02ebca93ed016
Paul2D <paul.maxineanu@gmail.com>
2022-09-13 18:20:14 +0300

    fix width bug

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="board">
-    <div class="flex flex-row items-start">
+    <div class="flex flex-row items-start flex-wrap">
       <board-column
         v-for="(column, columnIndex) in board.columns"
         :key="columnIndex"
